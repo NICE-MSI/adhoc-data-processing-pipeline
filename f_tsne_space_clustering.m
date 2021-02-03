cd7c4709d5c81bf041d588acfd39281579c5afad
Teresa Murta <teresa.murta@npl.co.uk>
2021-02-03 16:52:31 +0000

    Create f_tsne_space_clustering.m
    This was created to run after the data dimentionality reduction tnse, to cluster the transformed data.

---
+++ f_tsne_space_clustering.m
@@ -0,0 +1,26 @@
+function [ idx, tsne_colomap, optimal_numComponents, evaluation ] = f_tsne_space_clustering( reduced_x, clusters_num )
+
+% This function segments the t-sne embedding space using k-means. See
+% f_kmeans for more details regarding the segmentation.
+%
+% Inputs:
+% reduced_x - data in the reduced (tsne) space
+% clusters_num - number of clusters (can be an integer or NaN)
+% Outputs:
+% idx - cluster labels
+% tsne_colormap - map to use when plotting the segmentation results
+% optimal_numComponents - optimal number of clusters based on Elbow method
+% evaluation - a few metrics regarding the selection of the optimal number
+% of clusters
+
+% Clustering the t-sne space
+
+[ idx, C, optimal_numComponents, evaluation ] = f_kmeans( reduced_x, clusters_num, 'sqeuclidean' );
+
+C(C<0) = 0;
+C(C>1) = 1;
+
+tsne_colomap(1,1:3) = 0;
+tsne_colomap(2:max(idx)+1,:) = C;
+
+end
