0ede7cbf214d78d1d99a974ffda3393863b49921
Teresa Murta <teresa.murta@npl.co.uk>
2021-02-10 13:28:08 +0000

    Update f_tsne.m
    Correlation only, Exag and Perp tested with 3 different values each.

--- f_tsne.m
+++ f_tsne.m
@@ -23,43 +23,28 @@ end
 
 function [ y, loss, tsne_parameters ] = f_tsne_with_diff_parameters( x )
 
-step = ceil(1/1000*size(x,1)); % 0.1% of the original data
+step = ceil(1/5000*size(x,1)); % 0.5% of the original data
+NumDimensions = 3;
 
 small_x = x(step:step:(end-step),:);
 
 loss0 = Inf;
-
-for Perplexity = [ 30, 50, 100 ]
-    for Distance = [ "euclidean", "cosine" , "correlation" ]
-        for Exaggeration = [ 4, 8, 16, 32 ] % size of natural clusters in data
-            for NumDimensions = 3 % 3D space
-                for NumPCAComponents = [ 3, 10, 50, 0 ]
-                    
-                    if NumPCAComponents > size(x,2); NumPCAComponents = size(x,2); end
-                    
-                    for Standardize = 0 % logical([ 0, 1 ])
-                        
-                        description = join([ '! Distance: ' , Distance, ' Exaggeration: ', num2str(Exaggeration), ' NumDimensions: ' , num2str(NumDimensions), ' NumPCAComponents: ', num2str(NumPCAComponents), ' Perplexity: ', num2str(Perplexity), ' Standardize: ', num2str(Standardize) ]);
-                        disp(description)
-                        
-                        [ ~, loss1 ] = tsne( small_x, 'Distance' , char(Distance), 'Exaggeration', Exaggeration, 'NumDimensions' , NumDimensions, 'NumPCAComponents', NumPCAComponents, 'Perplexity', Perplexity, 'Standardize', Standardize );
-                        
-                        disp([ '* Loss: ' num2str(loss1) ])                        
-                        
-                        if loss1<loss0
-                            
-                            loss0 = loss1;
-                            
-                            tsne_parameters.distance = Distance;
-                            tsne_parameters.exaggeration = Exaggeration;
-                            tsne_parameters.numdimensions = NumDimensions;
-                            tsne_parameters.numPCAcomponents = NumPCAComponents;
-                            tsne_parameters.perplexity = Perplexity;
-                            tsne_parameters.standardize = Standardize;
-                            
-                        end
-                    end
-                end
+for Distance = "correlation" % [ "euclidean", "cosine" , "correlation" ]
+    for Exaggeration = [ 4, 16, 64 ] % size of natural clusters in data
+        for Perplexity = [ 50, 100, 500 ]
+            
+            disp(join([ '! Distance: ' , Distance, ' Exaggeration: ', num2str(Exaggeration), ' Perplexity: ', num2str(Perplexity)]))
+            
+            [ ~, loss1 ] = tsne( small_x, 'NumDimensions' , NumDimensions, 'Distance' , char(Distance), 'Exaggeration', Exaggeration, 'Perplexity', Perplexity );
+            
+            disp([ '* Loss: ' num2str(loss1) ])
+            
+            if loss1<loss0                
+                loss0 = loss1;
+                tsne_parameters.distance = Distance;
+                tsne_parameters.exaggeration = Exaggeration;
+                tsne_parameters.numdimensions = NumDimensions;
+                tsne_parameters.perplexity = Perplexity;
             end
         end
     end
@@ -67,15 +52,11 @@ end
 
 Distance = tsne_parameters.distance;
 Exaggeration = tsne_parameters.exaggeration;
-NumDimensions = tsne_parameters.numdimensions;
-NumPCAComponents = tsne_parameters.numPCAcomponents;
 Perplexity = tsne_parameters.perplexity;
-Standardize = tsne_parameters.standardize;
 
-description = join([ '! Selected parameters: Distance: ' , Distance, ' Exaggeration: ', num2str(Exaggeration), ' NumDimensions: ' , num2str(NumDimensions), ' NumPCAComponents: ', num2str(NumPCAComponents), ' Perplexity: ', num2str(Perplexity), ' Standardize: ', num2str(Standardize) ]);
-disp(description)
+disp(join([ '! (selected parameters) Distance: ' , Distance, ' Exaggeration: ', num2str(Exaggeration), ' Perplexity: ', num2str(Perplexity)]))
 
-[ y, loss ] = tsne( x, 'Distance' , char(Distance), 'Exaggeration', Exaggeration, 'NumDimensions' , NumDimensions, 'NumPCAComponents', NumPCAComponents, 'Perplexity', Perplexity, 'Standardize', Standardize );
+[ y, loss ] = tsne( x, 'NumDimensions' , NumDimensions, 'Distance' , char(Distance), 'Exaggeration', Exaggeration, 'Perplexity', Perplexity );
 
 disp([ '* Loss: ' num2str(loss) ]) 
 
