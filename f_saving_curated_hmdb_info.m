022f2b3aa5062e51d3425165e46a55acce6540c7
Teresa Murta <teresa.murta@npl.co.uk>
2021-03-22 18:15:08 +0000

    Update f_saving_curated_hmdb_info.m

--- f_saving_curated_hmdb_info.m
+++ f_saving_curated_hmdb_info.m
@@ -25,19 +25,19 @@ i = 1;
 starti = 1;
 new_hmdb_sample_info = strings(length(log_diff),10);
 for endi = log_diff
-        
+    
     % molecule
     
     long_string0 = unique(hmdb_sample_info2(starti:endi,1),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
     long_string = char(strjoin(long_string(1:end-1)));
     long_string = long_string(1,1:min(size(long_string,2),32000)); % to avoid the excel cell breaking issue
-    new_hmdb_sample_info(i,1) = string(long_string); 
+    new_hmdb_sample_info(i,1) = string(long_string);
     
     % monoiso mz
-            
+    
     new_hmdb_sample_info(i,2) = num2str(unique(double(hmdb_sample_info2(starti:endi,12)),'stable'),'%1.12f');
-
+    
     % meas mz
     
     new_hmdb_sample_info(i,3) = unique(hmdb_sample_info2(starti:endi,4),'stable');
@@ -46,38 +46,63 @@ for endi = log_diff
     
     long_string0 = unique(hmdb_sample_info2(starti:endi,3),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,4) = strjoin(long_string(1:end-1)); 
+    new_hmdb_sample_info(i,4) = strjoin(long_string(1:end-1));
     
     % ppm
     
     long_string0 = unique(hmdb_sample_info2(starti:endi,8),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,5) = strjoin(long_string(1:end-1)); 
+    new_hmdb_sample_info(i,5) = strjoin(long_string(1:end-1));
     
     % database
-        
+    
     database_rows_mono = strcmpi(relevant_lists_sample_info(:,12),num2str(unique(double(hmdb_sample_info2(starti:endi,12)),'stable'),'%1.12f')); % Monoisotopic mass comparison
     
     long_string0 = unique(relevant_lists_sample_info(database_rows_mono,7),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
     new_hmdb_sample_info(i,6) = strjoin(long_string(1:end-1));
     
-    % classes
-        
+    % kingdom
+    
     long_string0 = unique(hmdb_sample_info2(starti:endi,14),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,7) = strjoin(long_string(1:end-1)); 
+    long_string = char(strjoin(long_string(1:end-1)));
+    if ~isempty(long_string)
+        long_string = long_string(1,1:min(size(long_string,2),32000)); % to avoid the excel cell breaking issue
+    end
+    new_hmdb_sample_info(i,7) = string(long_string);
+    
+    % super class
+    
     long_string0 = unique(hmdb_sample_info2(starti:endi,15),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,8) = strjoin(long_string(1:end-1));
+    long_string = char(strjoin(long_string(1:end-1)));
+    if ~isempty(long_string)
+        long_string = long_string(1,1:min(size(long_string,2),32000)); % to avoid the excel cell breaking issue
+    end
+    new_hmdb_sample_info(i,8) = string(long_string);
+    
+    % class
+    
     long_string0 = unique(hmdb_sample_info2(starti:endi,16),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,9) = strjoin(long_string(1:end-1));
+    long_string = char(strjoin(long_string(1:end-1)));
+    if ~isempty(long_string)
+        long_string = long_string(1,1:min(size(long_string,2),32000)); % to avoid the excel cell breaking issue
+    end
+    new_hmdb_sample_info(i,9) = string(long_string);
+    
+    % sub class
+    
     long_string0 = unique(hmdb_sample_info2(starti:endi,17),'stable');
     long_string = reshape([long_string0 repmat(", ",size(long_string0,1),1)]',1,[]);
-    new_hmdb_sample_info(i,10) = strjoin(long_string(1:end-1)); 
+    long_string = char(strjoin(long_string(1:end-1)));
+    if ~isempty(long_string)
+        long_string = long_string(1,1:min(size(long_string,2),32000)); % to avoid the excel cell breaking issue
+    end
+    new_hmdb_sample_info(i,10) = string(long_string);
     
     i = i + 1;
     starti = endi+1;
-
+    
 end
